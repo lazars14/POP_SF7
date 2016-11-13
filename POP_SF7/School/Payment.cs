3524f115e0a9aea3d07c8c5acb68f762c2418f53
zomlaaa95 <zomlaaa95@gmail.com>
2016-11-13 11:20:07 +0100

    Izmenjen model projekta, dodat datum uplati

--- POP_SF7/School/Payment.cs
+++ POP_SF7/School/Payment.cs
@@ -1,4 +1,6 @@
-﻿namespace POP_SF7
+﻿using System;
+
+namespace POP_SF7
 {
     class Payment
     {
@@ -6,14 +8,16 @@
         public Course Course { get; set; }
         public Student Student { get; set; }
         public double PaymentAmount { get; set; }
+        public DateTime PaymentDate { get; set; }
         public bool Deleted { get; set; }
 
-        public Payment(int paymentId, Course course, Student student, double paymentAmount, bool deleted)
+        public Payment(int paymentId, Course course, Student student, double paymentAmount, DateTime paymentDate, bool deleted)
         {
             Id = paymentId;
             Course = course;
             Student = student;
             PaymentAmount = paymentAmount;
+            PaymentDate = paymentDate;
             Deleted = deleted;
         }
     }
